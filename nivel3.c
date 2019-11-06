5220ca5401788e26539967060a641c14c4755a1f
Zer0 <zer0_e@protonmail.com>
2019-11-06 11:10:11 +0100

    finished nivel3

--- nivel3.c
+++ nivel3.c
@@ -7,7 +7,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 /* Las llamadas al sistema están recopiladas en la sección 2 de Man. 
  * Ejemplos en nuestra aventura: open(), close(), chdir(), getcwd(), 
  * dup(), dup2(), execvp(), exit(), fork(), getpid(), kill(), pause(), 
@@ -26,6 +27,7 @@ int internal_export(char **args);
 int internal_source(char **args); 
 int internal_jobs(char **args); 
 int imprimir_prompt();
+int external_command(char **args);
 
 int main(){
     char line[COMMAND_LINE_SIZE];
@@ -61,7 +63,9 @@ char *read_line(char *line){
 int execute_line(char *line){
     char *args[ARGS_SIZE];
     parse_args(args,line);
-    check_internal(args);
+    if (check_internal(args) == 0){
+        external_command(args);
+    }
     return 0;
 }
 int parse_args(char **args, char *line){
@@ -81,17 +85,16 @@ int parse_args(char **args, char *line){
 int check_internal(char **args){
     if (args[0]){ // Comprovam que el primer no es nul, strcmp donaria un error si fos així
         if (strcmp(args[0],"cd") == 0){
-           internal_cd(args);
+           return internal_cd(args);
         } else if (strcmp(args[0],"export") == 0){
-           internal_export(args);
+           return internal_export(args);
         } else if (strcmp(args[0],"source") == 0){
-           internal_source(args);
+           return internal_source(args);
         } else if (strcmp(args[0],"jobs") == 0){
-           internal_jobs(args);
+           return internal_jobs(args);
         } else if (strcmp(args[0],"exit") == 0){
            exit(0);
         }
-        return 1;
     }
     return 0;
 }
@@ -101,6 +104,27 @@ S'ha comprobat que funcioni per a directoris aparentment conflictius, un cas que
 cd Aventura2/'prueba dir'/p\r\u\e\b\a/\"/'pr\"ueba dir larga'/pr\'\"\\ueba/prueba\ dir
 que ens duu al directori /home/user/Aventura2/prueba dir/prueba/"/pr"ueba dir larga/pr'"\ueba/prueba dir
 */
+
+int external_command(char **args){
+    int status;
+    pid_t pid;
+    pid = fork();
+    if (pid == 0){
+        printf("[execute_line()→ PID hijo: %d]\n",getpid());
+        execvp(args[0],args);
+        fprintf(stderr,"%s: no se encontró la orden\n",args[0]);
+        exit(-1);
+    } else if (pid > 0){
+        printf("[execute_line()→ PID padre: %d]\n",getpid());
+        wait(&status);
+        printf("[execute_line()→ Proceso hijo %d finalizado con exit(), estado: %d]\n",pid,WEXITSTATUS(status));
+
+    } else {
+        fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+        exit(-1);
+    }
+    return 0;
+}
 
 int internal_cd(char **args){
     char arg[COMMAND_LINE_SIZE];
@@ -145,22 +169,30 @@ int internal_cd(char **args){
 int internal_export(char **args){
     int error = 1;
     char *env[2];
-    if (args[1]){
-        env[0] = strtok(args[1],"=");
-        if (env[1] = strtok(NULL,""))
-            error = 0;
-    }
-    if (error)
-        fprintf(stderr, "Error de sintaxis. Uso: export Nombre=Valor\n");
-    else{
+    env[0] = strtok(args[1],"=");
+    if (env[1] = strtok(NULL,"")){
         if (setenv(env[0],env[1],1) == -1)
             fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
     }
+    else
+        fprintf(stderr, "Error de sintaxis. Uso: export Nombre=Valor\n");
     return 1;
 }
 
 int internal_source(char **args){
-    printf("[internal_source()→Esta función ejecutará un fichero de líneas de comandos]\n");
+    if (args[1]){
+        char line[COMMAND_LINE_SIZE];
+        FILE *fp;
+        if (fp = fopen(args[1],"r")){
+            while (fgets(line,150, fp)){
+                fflush(fp);
+                execute_line(line);
+            }
+        fclose(fp);
+        } else
+            fprintf(stderr,"Error: No se ha encontrado el archivo '%s'\n",args[1]);
+    } else
+        fprintf(stderr,"Error de sintaxis. Uso: source <nombre_fichero>\n");
     return 1;
 }
 
