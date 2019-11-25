6a65102ef2f67a96d5808e10687f43966fb19fa7
Toni Torres <zer0_e@protonmail.com>
2019-11-25 14:19:08 +0100

    commented most of nivel5.c

--- nivel5.c
+++ nivel5.c
@@ -1,5 +1,27 @@
 #include "nivel5.h"
 
+
+/*
+ * En aquest codi hem considerat que quan tornam 0 és success i 1 és failure.
+ */
+
+/*
+ * int main
+ * --------
+ * Controlarem les senyals SIGCHLD, SIGINT i SIGTSTP amb les seves funcions
+ * corresponents. Aquestes senyals venen donades per quan un fill acaba, quan
+ * pulsam CTRL+C i quan pulsam CTRL+Z respectivament.
+ * 
+ * Inicialitzam les variables globals g_argv, n_pids i la estructura jobs_list.
+ * g_argv contendrà el nom del programa que inicialitza es shell, n_pids el
+ * número total de treballs en background i la estructura jobs_list contendrà
+ * informació de cada treball (pid, estatus i la seva línia de comando),
+ * reservarem la posició [0] per guardar dades del procés en foreground.
+ * 
+ * Entram en un bucle del que només podrem sortir amb el comando exit o CTRL+D
+ * quan ens ensenya es prompt.
+ */
+
 int main(int argc, char *argv[]){
     char line[COMMAND_LINE_SIZE];
     signal(SIGCHLD, reaper);
@@ -15,12 +37,10 @@ int main(int argc, char *argv[]){
 }
 
 /*
- * int imprimir_prompt()
- * 
- * Amb la funció getenv("USER") i getcwd podem contruir el nostre prompt.
- * A més tal i com fa bash si el nostre directori comença per el directori
- * /home/user ho substituirem per ~
- * 
+ * int imprimir_prompt
+ * -------------------
+ * Amb la funció getenv("USER") i getcwd podem conseguir l'usuari i el directori
+ * al que esteim, amb això podem contruir i imprimir el nostre prompt.
  */
 int imprimir_prompt(){
     char *user = getenv("USER"), *home = getenv("HOME"), pwd[COMMAND_LINE_SIZE];
@@ -36,11 +56,14 @@ int imprimir_prompt(){
 }
 
 /*
- * char *read_line(char *line)
+ * char *read_line
+ * --------------- 
+ * Crida a imprimir_prompt i llegeix una línia de la consola amb fgets(), si 
+ * pulsam CTRL+D al principi d'una línia significa el final de la entrada stdin,
+ * per tant hem de controlar que si el resultat de fgets() és NULL i s'ha
+ * produit un EOF sortirem del shell amb exit.
  * 
- * Crida a imprimir_prompt i llegeix una línia de la consola
  * Torna el punter a la línia que hem llegit
- * 
  */
 char *read_line(char *line){
     char * ptr;
@@ -49,9 +72,9 @@ char *read_line(char *line){
     ptr = fgets (line, COMMAND_LINE_SIZE, stdin);
     if (!ptr) {
         printf("\r");
-       if (feof(stdin)) { // Si s'ha produit un EOF (hem pulsat CTRL+D) sortirem del shell
-           imprimir_prompt(); // tal i com tenim a bash ens monstra el prompt + exit quan sortim amb ctrl+d
-           printf("exit\n"); 
+       if (feof(stdin)) {
+           imprimir_prompt();
+           printf("exit\n"); // mostrarem exit abans de sortir.
            exit(0);
        } else {
            ptr = line;
@@ -62,12 +85,13 @@ char *read_line(char *line){
 }
 
 /*
- * int execute line(char *line)
- * 
- * Cridarà a parse args per obtenir la línia que l'usuari ha introdui fragmentada en tokens.
- * Passam els tokens a la función boolena check_internal() perquè ens determini si és un comand
- * intern o extern * 
-*/
+ * int execute line
+ * ----------------
+ * Cridarà a parse args per obtenir la línia que l'usuari ha introduit
+ * fragmentada en tokens.
+ * Passam els tokens a la función boolena check_internal() perquè ens determini 
+ * si és un comando intern o extern i l'executarà de forma adecuada.
+ */
 int execute_line(char *line){
     char *args[ARGS_SIZE];
     strcpy(jobs_list[0].command_line,line);
@@ -81,15 +105,22 @@ int execute_line(char *line){
     return 0;
 }
 
-
 /*
- * int parse_args(char **args, char *line)
+ * int parse_args
+ * --------------
+ * Trosseja la línia obtinguda mitjançant la funció strtok(). Ignorarà els 
+ * comentaris (els que comencen per #) i afegirà com a darrer argument a 
+ * l'string de caràcters NULL. Així en altres funcions podrem saber quan acaba 
+ * es bucle de llegir tokens.
  * 
- * Trosseja la línia obtinguda mitjançant la funció strtok(). Ignorarà els comentaris
- * que comencin per # i afegirà com a darrer argument a l'string de caràcters NULL.
- * Així en altres funcions podrem saber quan acaba es loop de llegir tokens.
- *  
+ * Exemples:
+ *      pwd              -> ["pwd",NULL]
+ *      pwd #comentari   -> ["pwd",NULL]
+ *      pwd no#comentari -> ["pwd","no#comentari", NULL]
+ * 
+ * Torna el número de tokens sense contar es darrer (NULL)
  */ 
+
 int parse_args(char **args, char *line){
     int token_counter = 0;
     char *token, delim[5] = " \t\r\n";
@@ -104,11 +135,15 @@ int parse_args(char **args, char *line){
     return token_counter;
 }
 
-
 /*
- * int check_internal(char **args)
- * És una funció booleana que troba si args[0] és un ordre interna, si ho és l'executarà i tornarà 0.
+ * int check_internal
+ * ------------------
+ * És una funció booleana que troba si args[0] és un comando intern amb strcmp()
+ * si ho és l'executarà.
+ * 
+ * Tornarà 0 si és un comando intern i 1 en casd contrari.
  */
+
 int check_internal(char **args){
     if (strcmp(args[0],"cd") == 0){
         internal_cd(args);
@@ -133,23 +168,27 @@ int check_internal(char **args){
 //     INTERNAL COMMANDS
 
 /*
-S'ha comprobat que funcioni per a directoris aparentment conflictius, un cas que funciona com a bash és:
-cd Aventura2/'prueba dir'/p\r\u\e\b\a/\"/'pr\"ueba dir larga'/pr\'\"\\ueba/prueba\ dir
-que ens duu al directori /home/user/Aventura2/prueba dir/prueba/"/pr"ueba dir larga/pr'"\ueba/prueba dir
-*/
-
-/*
- * int internal_cd(char **args)
- * Utilitza la cridad al sistema chdir() per canviar de directori. Si l'argument de cd té \ , " o '
- * haurem de tractar els arguments que ens han passat.
- * El caràcter \ s'eliminarà i deixarem el caràcter que vengui després d'ell (encara que sigui una altra \).
- * A més si el possam al final d'una parauala indicam que volem un espai.
- * Tant " com ' indicaran el principi i el final d'un directori que contengui un nombre ams espais 
+ * int internal_cd
+ * ---------------
+ * Utilitza la cridad al sistema chdir() per canviar de directori.
+ * 
+ * Si l'argument té \, " o ' haurem de tractar els arguments que ens han passat.
+ * 
+ * El caràcter \ s'eliminarà i deixarem el caràcter que vengui després d'ell 
+ * (encara que sigui un altre \). A més si el possam al final d'una parauala
+ * indicam que volem un espai.
+ * 
+ * Tant " com ' indicaran el principi i el final d'un directori que contengui un
+ * nombre ams espais.
  * 
  * Si cd no té arguments canviarem al directori HOME.
- *  
- * exemple: cd sistemes\ operatius, cd 'sistemes operatius' i cd "sistemes operatius" ens duran al mateix directori que se diu <prueba dir>
-*/
+ * 
+ * Exemples per accedir a un directori anomenat "sistemes operatius":
+ *      cd sistemes\ operatius
+ *      cd 'sistemes operatius'
+ *      cd "sistemes operatius"
+ */
+
 int internal_cd(char **args){
     char arg[COMMAND_LINE_SIZE];
     memset(arg,0,sizeof(arg));
@@ -190,12 +229,16 @@ int internal_cd(char **args){
     return 0;
 }
 
-
 /*
- * internal_export(char **args)
- * Descompossa en tokens l'argument NOM=VALOR. Si la sintaxis és incorrecta li notificarem a l'usuari.
- * Mitjançant la funció setenv() assignarem un valor a una variable d'entorn
-*/
+ * internal_export
+ * ---------------
+ * Descompossa en tokens l'argument NOM=VALOR. Si la sintaxis és incorrecta li
+ * notificarem a l'usuari.
+ * 
+ * Mitjançant la funció setenv() assignarem un valor (VALOR) a una variable 
+ * d'entorn (NOM).
+ */
+
 int internal_export(char **args){
     int error = 1;
     char *env[2];
@@ -210,12 +253,14 @@ int internal_export(char **args){
 }
 
 /*
- * internal_source(char **args)
+ * internal_source
+ * --------------- 
+ * Aquesta funció obrirà un arxiu amb fopen(), llegirà línia a línea l'arxiu
+ * mitjançant fgets() i la passarem a la funció execute_line(). Al acabar de
+ * llegir l'arxiu el tancam amb fclose().
  * 
- * Aquesta funció obrirà un arxiu i executarà les ordres que hi ha en aquest línia per línia.
- * 
- * 
-*/
+ * Si la sintaxis és incorrecta li notificarem a l'usuari.
+ */
 
 int internal_source(char **args){
     if (args[1]){
@@ -228,7 +273,8 @@ int internal_source(char **args){
             }
         fclose(fp);
         } else
-            fprintf(stderr,"Source: No se ha encontrado el archivo '%s'\n",args[1]);
+            fprintf(stderr,"Source: No se ha encontrado el archivo '%s'\n",
+                    args[1]);
     } else
         fprintf(stderr,"Error de sintaxis. Uso: source <nombre_fichero>\n");
     return 0;
@@ -236,33 +282,40 @@ int internal_source(char **args){
 
 
 /*
- * int internal_jobs(char **args)
- * Si no hi ha cap argument imprimirà per pantalla l'identificador del treball, el PID, la línia de comands i l'status de tots els treballs que tenim actualment.
- * Si afegim un argument només imprimirà el treball que li hem indicat mitjançant aquest.
-*/
+ * internal_jobs
+ * -------------
+ * Si no hi ha cap argument imprimirà per pantalla l'identificador del treball,
+ * el PID, la línia de comands i l'status de tots els treballs que tenim.
+ * 
+ * Si afegim un argument imprimirà el treball que li hem indicat mitjançant 
+ * aquest.
+ */
+
 int internal_jobs(char **args){
     if (args[1]){
         int pos = atoi(args[1]);
         if (pos < 1 || pos > n_pids){
             fprintf(stderr,"jobs %s: no existe tal trabjo\n",args[1]);
         } else {
-            printf("[%d] %d\t%c\t%s\n",pos,jobs_list[pos].pid,jobs_list[pos].status,jobs_list[pos].command_line);
+            printf("[%d] %d\t%c\t%s\n",pos,jobs_list[pos].pid,
+                    jobs_list[pos].status,jobs_list[pos].command_line);
         }
     } else {
         for (int i = 1; i <= n_pids; i++){
-            printf("[%d] %d\t%c\t%s\n",i,jobs_list[i].pid,jobs_list[i].status,jobs_list[i].command_line);
+            printf("[%d] %d\t%c\t%s\n",i,jobs_list[i].pid,jobs_list[i].status,
+                    jobs_list[i].command_line);
         }
     }
     return 0;
 }
 
 /*
-internal_fg(char **args)
-    Mou un treball a foreground.
+ * int internal_fg
+ * ---------------
+ * Possa el treball que li hem indicat amb <fg jobID> a foreground, si estaba
+ * detingut, el resumim.
+ */
 
-    Possa el treball identificat per args[1] (JOB_SPEC) a foreground.
-    Si JOB_SPEC no està present o 
-*/
 int internal_fg(char **args){
     if (args[1]){
         int pos = atoi(args[1]);
@@ -273,7 +326,8 @@ int internal_fg(char **args){
         if (jobs_list[pos].status == 'D'){
             jobs_list[pos].status = 'E';
             if (kill(jobs_list[pos].pid,18) == 0){
-                printf("[internal_fg()→ Señal 18 (SIGCONT) enviada a %d (%s)]\n",jobs_list[pos].pid,jobs_list[pos].command_line);
+                printf("[internal_fg()→ Señal 18 (SIGCONT) enviada a %d (%s)]\n"
+                        ,jobs_list[pos].pid,jobs_list[pos].command_line);
             } else {
                 perror("kill");
             }
@@ -291,9 +345,21 @@ int internal_fg(char **args){
         fprintf(stderr, "Error de sintaxis. Uso: fg <job_id>\n");
     }
     return 0;
-} // Que feim en el cas 'fg 1 asjdbisabdisbixoa', donam syntax error o feim simplement fg 1 i ignoram lo que ve després d'aquest
-  // INFO: a bash ignoram lo que ve després del número
+} // IMPORTANT implementar que pugui agafar més d'un jobID
+  // per exemple fer fg jobID1 jobID2 jobID3 POSSARHO A README.TXT AL ACABAR!"!!!!!!!!!"
+
+
+
+
+
   // Cas sleep 20 &sleep 20 a bash cream un procés a background i un a foreground, que feim aqui? modificar parse_args?
+
+/*
+ * int internal_bg
+ * ---------------
+ * Si el treball que li hem indicat està detingut el resumim i el deixam a
+ * background.
+ */
 
 int internal_bg(char **args){
     if (args[1]){
@@ -320,10 +386,32 @@ int internal_bg(char **args){
     return 0;
 }
 
+/*
+ * int external_command
+ * --------------------
+ * Comprobarà si el nostre comando és background i farà el control d'errors
+ * corresponent. Cridarem a fork per crear un fill.
+ * 
+ * El procés fill:
+ *      Ignorarà la interrupció CTRL+C, executarà l'acció per defecte per a la
+ *      senyal SIGCHLD i ignorarà la senyal CTRL+Z si és un proces en background
+ *      i executarà l'acció per defecte si és en foreground. El pare sirà
+ *      l'encarregat de tractar totes aquestes senyals.
+ * 
+ *      Cridarà a la funció execvp() per executar el comando extern solicitat.
+ * 
+ * El procés pare:
+ *      Si el comando és background l'afegirem a la llista de treballs amb la
+ *      funció jobs_list_add().
+ * 
+ *      Si no, donarà els valors del procés en foreground i executarà un pause()
+ *      mentres hi hagui un procés executant-se en foreground.
+ */
+
 int external_command(char **args){
     pid_t pid;
     int bkg = is_background(args);
-    if (bkg && n_pids >= N_JOBS -1){
+    if (bkg == 0 && n_pids >= N_JOBS -1){
         fprintf(stderr,"Error, no se pueden añadir más procesos en segundo plano\n");
         return 1;
     }
@@ -331,8 +419,10 @@ int external_command(char **args){
     if (pid == 0){
         signal(SIGCHLD,SIG_DFL);
         signal(SIGINT, SIG_IGN); // Ignoram senyal ctrl+c general
-        if (bkg){
+        if (bkg == 0){
             signal(SIGTSTP,SIG_IGN);
+        } else{
+            signal(SIGTSTP,SIG_DFL); // ATENCIO!!!!! toca comprobar si llevant lo del controlador funciona bé!!!!
         }
         execvp(args[0],args);
         fprintf(stderr,"%s: no se ha encontrado el comando\n",args[0]);
@@ -340,9 +430,8 @@ int external_command(char **args){
     } else if (pid > 0){
         printf("[execute_line()→ PID padre: %d (%s)]\n",getpid(),g_argv);
         printf("[execute_line()→ PID hijo: %d (%s)]\n",pid,jobs_list[0].command_line);
-        if (bkg){
+        if (bkg == 0){
             jobs_list_add(pid,'E',jobs_list[0].command_line);
-            memset(jobs_list[0].command_line,0,sizeof(jobs_list[0].command_line));
         } else{
             jobs_list[0].pid = pid;
             while (jobs_list[0].pid)
@@ -355,18 +444,42 @@ int external_command(char **args){
     return 0;
 }
 
+/*
+ * int is_background
+ * -----------------
+ * Analitza a la línea de comandos si hi ha un & al final.
+ * 
+ * Torna 0 si és background i 1 si no ho és.
+ */
+
 int is_background(char **args){
-    for (int i = 1; args[i] ; i++){
+    /*for (int i = 1; args[i] ; i++){
         if (args[i][0] == '&'){
             args[i] = NULL;
             return 1; // True
         }
     }
     return 0; // False
+    */
+   int position_to_check = parse_args(args,jobs_list[0].command_line);
+   if (strcmp(args[position_to_check],"&" == 0)){
+       return 0; // True
+   } else {
+       return 1; // False
+   }
 }
+
+/*
+ * int jobs_list_add
+ * -----------------
+ * Afegim un nou element a l'array de jobs_list indicada per la variable global
+ * n_pids
+ */
 
 int jobs_list_add(pid_t pid, char status, char *command_line){
     n_pids++;
+    jobs_list[0].pid = 0;
+    memset(jobs_list[0].command_line,0,sizeof(jobs_list[0].command_line));
     jobs_list[n_pids].pid = pid;
     jobs_list[n_pids].status = status;
     strcpy(jobs_list[n_pids].command_line,command_line);
@@ -375,6 +488,14 @@ int jobs_list_add(pid_t pid, char status, char *command_line){
 } // Aquí no tenim condicionals perquè en casos on la llista de jobs estigui plena ens interesa saber-ho abans de cridar al comand jobs_list_add i
   // mai el cridarem si ja esteim en es límit de jobs. Així si volem fer un procés en background i ja esteim al límit ni tal sols cridarem en es fork.
   // Si pulsam ctrl+z i esteim plens, no l'aturarem.
+
+/*
+ * int jobs_list_find
+ * ------------------
+ * Cerca a l'array del treball el PID que agafa com argument.
+ * 
+ * Torna la posició en que es troba aquest PID, si no l'ha trobat tornarà 0.
+ */
 
 int jobs_list_find(pid_t pid){
     int position = 1;
@@ -387,7 +508,15 @@ int jobs_list_find(pid_t pid){
               // un procés que no estigui a la llista.
 }
 
-int  jobs_list_remove(int pos){
+/*
+ * int jobs_list_remove
+ * --------------------
+ * Agafa com argument la posició de l'array del treball que hem d'eliminat i mou
+ * el registre del darrer procés de la llista a la posició que hem eliminat.
+ * Reduim el valor de n_pids per 1.
+ */
+
+int jobs_list_remove(int pos){
     jobs_list[pos] = jobs_list[n_pids];
     jobs_list[n_pids].pid = 0;
     n_pids--;
@@ -403,7 +532,12 @@ Revisar si totes les cridades al sistema tenen un control d'errors (al acabar to
     Aquestes son open(), close(), chdir(), getcwd(), dup(), dup2(), execvp(), exit(), fork(), getpid(), kill(), pause(), 
     signal(), wait(), waitpid(). Las llamadas al sistema están recopiladas en la sección 2 de Man
 Revisar internal_cd i provar més casos.
-README.txt
+Acabar README.txt
+
+Implementar flags per a funcions internes.
+implementar que internal_fg pugui agafar més d'un jobID i la possibilitat de possar un string
+
+Possible solució fer que el fill sempre ignori SIGTSTP
 */
 
 //      CONTROLADORS
@@ -468,8 +602,6 @@ void ctrlz(int signum){
             } else if(kill(jobs_list[0].pid,20) == 0){
                 printf("[ctrlz()→ Señal 20 (SIGTSTP) enviada a %d (%s) por %d (%s)]\n",jobs_list[0].pid,jobs_list[0].command_line,getpid(),g_argv);
                 jobs_list_add(jobs_list[0].pid,'D',jobs_list[0].command_line);
-                jobs_list[0].pid = 0;
-                memset(jobs_list[0].command_line,0,sizeof(jobs_list[0].command_line));
             } else {
                 perror("kill");
             }
